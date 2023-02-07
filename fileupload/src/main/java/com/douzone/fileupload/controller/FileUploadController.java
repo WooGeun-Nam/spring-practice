6e697284db9f911275c2c78b31730e2921557e92
WooGeun-Nam <rolru7686@gmail.com>
2023-02-07 14:54:51 +0900

    file upload #finish

--- fileupload/src/main/java/com/douzone/fileupload/controller/FileUploadController.java
+++ fileupload/src/main/java/com/douzone/fileupload/controller/FileUploadController.java
@@ -1,12 +1,18 @@
 package com.douzone.fileupload.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.douzone.fileupload.service.FileuploadService;
+
 @Controller
 public class FileUploadController {
+	@Autowired
+	private FileuploadService fileuploadService;
 	
 	@RequestMapping({"", "/form"})
 	public String form() {
@@ -17,8 +23,15 @@ public class FileUploadController {
 	@RequestMapping("/upload")
 	public String upload(
 		@RequestParam("email") String email, 
-		@RequestParam("file") MultipartFile file) {
-		// String url = fileuploadService.restore(file);
+		@RequestParam("file") MultipartFile file,
+		Model model) {
+		
+		System.out.println("email:"+email);
+		
+		String url = fileuploadService.restore(file);
+		model.addAttribute("url", url);
+		
+		System.out.println(url);
 		
 		return "WEB-INF/views/result.jsp";
 	}
