e93d8d8974e7cc346b8c6b15a60a06eee65407db
WooGeun-Nam <rolru7686@gmail.com>
2023-02-10 13:03:31 +0900

    container - user

--- container/src/main/java/com/douzone/container/user/config/AppConfig.java
+++ container/src/main/java/com/douzone/container/user/config/AppConfig.java
@@ -4,14 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.douzone.container.user.User;
+import com.douzone.container.user.Friend;
 
 @Configuration
+// auto scanning
 @ComponentScan(basePackages={"com.douzone.container.user"})
 public class AppConfig {
-	
+
+//  명시적 설정
 //	@Bean
 //	public User user() {
 //		return new User();
 //	}
+	
+	@Bean
+	public Friend friend() {
+		return new Friend("Loopy");
+	}
 }
