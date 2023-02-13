e3b899dc12291f38e11826279c3898a90cd3a3da
WooGeun-Nam <rolru7686@gmail.com>
2023-02-13 13:28:21 +0900

    Bean Configuration #finish

--- container/src/main/java/com/douzone/container/config/videosystem/mixing/DVDConfig.java
+++ container/src/main/java/com/douzone/container/config/videosystem/mixing/DVDConfig.java
@@ -4,13 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.douzone.container.videosystem.Avengers;
+import com.douzone.container.videosystem.BlankDisc;
 import com.douzone.container.videosystem.DigitalVideoDisc;
 
 @Configuration
 public class DVDConfig {
-	
+
 	@Bean
 	public DigitalVideoDisc avenger() {
 		return new Avengers();
 	}
+	
+	@Bean
+	public DigitalVideoDisc avengersInfinityWar() {
+		BlankDisc dvd = new BlankDisc();
+		dvd.setTitle("Avengers Infinity War");
+		dvd.setStudio("MARVEL");
+		
+		return dvd;
+	}
+	
 }
